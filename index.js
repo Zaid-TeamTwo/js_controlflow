0348e2c0d3bcf3ff4871ea47888667d20e517d51
Zaid <zaidal.lalikai@gmail.com>
2025-12-04 16:11:55 +0700

    feat: convert pseudocode krusty krab restaurant

---
+++ index.js
@@ -0,0 +1,23 @@
+let nameRestaurant = "Krusty Krab";
+let ingredients = "bread, tomatoes, beef, and plankton";
+let purchasePerDay = 600; // contoh nilai pembelian per hari
+
+function getEmployeeOfTheMonth() {
+    return{
+        name: "Spongebob",
+        age: 12
+    };
+}
+
+// cek apakah reesp enak
+if (purchasePerDay>500) {
+    console.log("Resepnya enak dan lezat");
+} else {
+    console.log("Resepnya standar dan biasa");
+}
+
+let employee = getEmployeeOfTheMonth();
+
+console.log("Restaurant:", nameRestaurant);
+console.log("Ingredients:", ingredients);
+console.log("Employee of The Month:", employee.name, "- Age", employee.age);
